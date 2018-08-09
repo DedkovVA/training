c23740e116f136c97eb5bb5cc67aa5cfed18bd3a
sbt-dedkov-va <vadedkov.sbt@sberbank.ru>
2018-08-09 10:36:18 +0300

    roll implementation

--- src/main/scala/dedkovv/Game.scala
+++ src/main/scala/dedkovv/Game.scala
@@ -1,41 +1,60 @@
 package dedkovv
 
-class Game {
+class Game(val lastFrameInd: Int = 9) {
   type FrameInd = Int
   type Score = Int
 
   val maxPins = 10
-  val maxNumOfTry = 2
+  val maxNumOfTries = 2
+  val maxNumOfTriesInLastFrame = 3
 
   var rollInd: Int = -1
   var frameInd: FrameInd = 0
   var frameToPins = Map.empty[FrameInd, Seq[Score]]
+  var isFinished = false
 
   var scoreN = 0
 
   def roll(pins: Int): Unit = {
     require(pins >= 0 && pins <= 10)
 
+    assert(!isFinished)
+
     rollInd += 1
+
     if (frameToPins.contains(frameInd)) {
       val scoresPerFrameSeq = frameToPins(frameInd)
 
-      assert(scoresPerFrameSeq.nonEmpty && scoresPerFrameSeq.size <= maxNumOfTry)
+      assert(scoresPerFrameSeq.nonEmpty && (scoresPerFrameSeq.size <= maxNumOfTries))
 
       val scoresPerFrame = scoresPerFrameSeq.sum
 
-      if (scoresPerFrame == maxPins || scoresPerFrameSeq.size == maxNumOfTry) {
+      if (frameInd == lastFrameInd) {
+        val rem = remainder(scoresPerFrameSeq)
+
+        assert(rem == 0 || pins <= rem)
+
+        frameToPins += (frameInd -> (scoresPerFrameSeq :+ pins))
+      } else if (scoresPerFrame == maxPins || scoresPerFrameSeq.size == maxNumOfTries) {
         frameInd += 1
         updateFrameToPins(pins)
       } else {
-        val tail = maxPins - scoresPerFrameSeq.last
+        val rem = remainder(scoresPerFrameSeq)
 
-        assert(pins <= tail)
+        assert(pins <= rem)
 
         frameToPins += (frameInd -> (scoresPerFrameSeq :+ pins))
       }
     } else {
       updateFrameToPins(pins)
+    }
+
+    if (frameInd == lastFrameInd) {
+      val scoresPerFrameSeq = frameToPins(frameInd)
+      if (scoresPerFrameSeq.size == maxNumOfTriesInLastFrame ||
+        scoresPerFrameSeq.size == 2 && scoresPerFrameSeq.sum < maxPins) {
+        isFinished = true
+      }
     }
   }
 
@@ -44,6 +63,11 @@ class Game {
   private def updateFrameToPins(pins: Int): Unit = {
     frameToPins += (frameInd -> List(pins))
   }
+
+  private def remainder(scoresPerFrameSeq: Seq[Int]): Int = {
+    maxPins - scoresPerFrameSeq.last
+  }
+
 }
 
 object Game extends App {
--- src/test/scala/dedkovv/GameSpec.scala
+++ src/test/scala/dedkovv/GameSpec.scala
@@ -4,7 +4,11 @@ import org.scalatest.{FreeSpec, Matchers}
 
 class GameSpec extends FreeSpec with Matchers {
   "first" in {
-    val g = new Game
+    val g = new Game(5)
+
+    g.rollInd shouldBe -1
+    g.frameInd shouldBe 0
+    g.frameToPins.isEmpty shouldBe true
 
     g.roll(3)
     g.rollInd shouldBe 0
@@ -15,5 +19,50 @@ class GameSpec extends FreeSpec with Matchers {
     g.rollInd shouldBe 1
     g.frameInd shouldBe 0
     g.frameToPins(0) shouldBe Seq(3, 4)
+
+    g.roll(2)
+    g.rollInd shouldBe 2
+    g.frameInd shouldBe 1
+    g.frameToPins(0) shouldBe Seq(3, 4)
+    g.frameToPins(1) shouldBe Seq(2)
+
+    g.roll(8)
+    g.rollInd shouldBe 3
+    g.frameInd shouldBe 1
+    g.frameToPins(0) shouldBe Seq(3, 4)
+    g.frameToPins(1) shouldBe Seq(2, 8)
+
+    g.roll(10)
+    g.rollInd shouldBe 4
+    g.frameInd shouldBe 2
+    g.frameToPins(0) shouldBe Seq(3, 4)
+    g.frameToPins(1) shouldBe Seq(2, 8)
+    g.frameToPins(2) shouldBe Seq(10)
+
+    g.roll(10)
+    g.rollInd shouldBe 5
+    g.frameInd shouldBe 3
+    g.frameToPins(0) shouldBe Seq(3, 4)
+    g.frameToPins(1) shouldBe Seq(2, 8)
+    g.frameToPins(2) shouldBe Seq(10)
+    g.frameToPins(3) shouldBe Seq(10)
+
+    g.roll(1)
+    g.rollInd shouldBe 6
+    g.frameInd shouldBe 4
+    g.frameToPins(0) shouldBe Seq(3, 4)
+    g.frameToPins(1) shouldBe Seq(2, 8)
+    g.frameToPins(2) shouldBe Seq(10)
+    g.frameToPins(3) shouldBe Seq(10)
+    g.frameToPins(4) shouldBe Seq(1)
+
+    g.roll(0)
+    g.rollInd shouldBe 7
+    g.frameInd shouldBe 4
+    g.frameToPins(0) shouldBe Seq(3, 4)
+    g.frameToPins(1) shouldBe Seq(2, 8)
+    g.frameToPins(2) shouldBe Seq(10)
+    g.frameToPins(3) shouldBe Seq(10)
+    g.frameToPins(4) shouldBe Seq(1, 0)
   }
 }
